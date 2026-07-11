d75fb77c9486f5e2f41c81554b17651080d15b32
AhmadHirzallah <ahmad.hirzallah.pro@gmail.com>
2026-07-11 19:21:04 +0300

    (emailing): add email service abstraction and implementation
    - Add IEmailService contract in the Application layer
    - Add EmailService implementation in Infrastructure

---
+++ BookCart.Application/Common/Abstractions/Emailing/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace BookCart.Application.Common.Abstractions.Emailing;
+
+public interface IEmailService
+{
+    //! Replace [string to] with something like [EmailAddress to] in the future, but for now, we will keep it simple.
+    Task SendAsync(string to, string subject, string body, CancellationToken ct);
+}
---
+++ BookCart.Infrastructure/Services/Emailing/EmailService.cs
@@ -0,0 +1,11 @@
+﻿using BookCart.Application.Common.Abstractions.Emailing;
+
+namespace BookCart.Infrastructure.Services.Emailing;
+
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(string to, string subject, string body, CancellationToken ct)
+    {
+        return Task.CompletedTask;
+    }
+}
